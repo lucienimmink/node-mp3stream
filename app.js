ca84d71e643d6589363da20f2fa78190a1bd32bb
Lucien Immink <lucien.immink@isaac.nl>
2013-10-18 17:04:08 +0200

    updated streaming, added login check (though very flimsy atm)

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var settings = {
 	loggedIn : false
 };
 
-/* 
-	serve all files stored in the web folder as normal files; you can store the website that will use the streamer in this folder.
-	if you don't want this; please remove the next 2 lines.
-*/
-app.use(app.router); 
+/*
+ serve all files stored in the web folder as normal files; you can store the website that will use the streamer in this folder.
+ if you don't want this; please remove the next 2 lines.
+ */
+app.use(app.router);
 app.use(express.static(__dirname + "/web"));
 
 /**
@@ -16,37 +16,40 @@ app.use(express.static(__dirname + "/web"));
  */
 app.get('/listen', function(req, res) {
 	var path = req.query.path;
+	if (settings.loggedIn) {
+		fs.exists(path, function(exists) {
+			if (exists) {
+				console.log("going to stream", path);
+				fs.readFile(path, 'binary', function(err, file) {
+					var header = {};
+					var range = req.headers.range;
+					var parts = range.replace(/bytes=/, "").split("-");
+					var partialstart = parts[0];
+					var partialend = parts[1];
 
-	fs.exists(path, function(exists) {
-		if (exists) {
-			console.log("going to stream", path);
-			fs.readFile(path, 'binary', function(err, file) {
-				var header = {};
-				var range = req.headers.range;
-				var parts = range.replace(/bytes=/, "").split("-");
-				var partialstart = parts[0];
-				var partialend = parts[1];
+					var total = file.length;
 
-				var total = file.length;
+					var start = parseInt(partialstart, 10);
+					var end = partialend ? parseInt(partialend, 10) : total - 1;
 
-				var start = parseInt(partialstart, 10);
-				var end = partialend ? parseInt(partialend, 10) : total - 1;
+					header["Content-Range"] = "bytes " + start + "-" + end + "/" + (total);
+					header["Accept-Ranges"] = "bytes";
+					header["Content-Length"] = (end - start) + 1;
+					header["Connection"] = "close";
 
-				header["Content-Range"] = "bytes " + start + "-" + end + "/" + (total);
-				header["Accept-Ranges"] = "bytes";
-				header["Content-Length"] = (end - start) + 1;
-				header["Connection"] = "close";
-
-				res.writeHead(206, header);
-				res.write(file.slice(start, end) + '0', "binary");
-				res.end();
-				return;
-			});
-		} else {
-			console.error("no file with name", path, "found");
-		}
-	});
-
+					res.writeHead(206, header);
+					res.write(file.slice(start, end) + '0', "binary");
+					res.end();
+					return;
+				});
+			} else {
+				console.error("no file with name", path, "found");
+			}
+		});
+	} else {
+		res.writeHead(401);
+		res.end();
+	}
 });
 
 /**
