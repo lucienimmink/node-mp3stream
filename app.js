e80a6b7cffae14de56ac8b704332f674c1e4c2d0
Lucien Immink <lucien.immink@isaac.nl>
2013-11-07 13:19:53 +0100

    Added cache headers

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ logger.setLevel('INFO');
 /* CORS */
 var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Methods', 'GET');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+	res.setHeader("Cache-Control", "public, max-age=4320000"); // 12 hours
+    res.setHeader("Expires", new Date(Date.now() + 4320000).toUTCString());
 	// intercept OPTIONS method
 	if ('OPTIONS' == req.method) {
 		res.send(200);
