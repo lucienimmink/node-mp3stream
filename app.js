5af627ec0130b8be8d371be5c79f37938603152a
Lucien Immink <lucien@arielext.org>
2017-05-01 22:42:54 +0200

    [TASK] Set maasive cache for images as well

--- app.js
+++ app.js
@@ -372,6 +372,11 @@ if (config.ask || !config.path) {
                 res.end(e);
             }).on('response', function(r) {
                 delete r.headers['connection'];
+                var expires = new Date(Date.now());
+                expires.setDate(expires.getDate() + 1);
+                expires.setFullYear(expires.getFullYear() + 1);
+                r.headers['cache-control'] = `public, max-age=${expires.getTime()}`; // A year and a day
+                r.headers['expires'] = new Date(expires.getTime()).toUTCString();
             }).pipe(res);
         } else {
             res.end("no url found");
