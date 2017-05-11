b715db9ea6e17fa1e3acb446a07069564d820b83
Lucien Immink <lucien.immink@isaac.local>
2017-05-11 12:00:01 +0200

    [TASK] Fix progress scanner

--- endpoints/progress.js
+++ endpoints/progress.js
@@ -1,4 +1,5 @@
 var validateJwt = require('./../modules/validateJwt'),
+    fs = require('fs'),
     log4js = require('log4js');
 
 log4js.loadAppender('file');
@@ -6,20 +7,16 @@ log4js.addAppender(log4js.appenders.file('logs/mp3stream.log'), 'progress');
 var logger = log4js.getLogger('progress');
 logger.setLevel('INFO');
 
+var progressFile = './public/data/progress.txt';
+
 module.exports = function(req, res) {
     var jwt = req.query.jwt;
     validateJwt(jwt, function(val) {
         if (val) {
             // progress should be written to the output folder as a file progress.txt containing the actual percentage
-            var hasProgressFile = false;
-            try {
-                hasProgressFile = fs.statSync('public/data/progress.txt').isFile();
-            } catch (e) {
-                hasProgressFile = false;
-            }
-            // console.log('has progress file? ', hasProgressFile);
+            var hasProgressFile = fs.existsSync(progressFile);
             if (hasProgressFile) {
-                var progress = fs.readFileSync('public/data/progress.txt', 'utf8');
+                var progress = fs.readFileSync(progressFile, 'utf8');
                 res.statusCode = 200;
                 res.write(JSON.stringify({
                     progress: progress,
