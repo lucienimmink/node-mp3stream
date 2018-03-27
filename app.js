77b0b10ca7d8abce31dd1ac98d1a30cd57803543
Lucien Immink <lucien@arielext.org>
2018-03-27 22:02:57 +0200

    [TASK] Remove http2 module untill I read about to use it properly nowadays

--- app.js
+++ app.js
@@ -1,86 +1,99 @@
-var express = require('express'),
-    fs = require('fs'),
-    walk = require('fs-walk'),
-    compression = require('compression'),
-    http2 = require('http2'),
-    expressHTTP2Workaround = require('express-http2-workaround'),
-    log4js = require('log4js'),
-    cors = require('./modules/cors'),
-    cache = require('./modules/cache'),
-    imageProxy = require('./endpoints/imageProxy'),
-    rescan = require('./endpoints/rescan'),
-    progress = require('./endpoints/progress'),
-    login = require('./endpoints/login'),
-    listen = require('./endpoints/listen'),
-    config = require('./config.json'),
-    ask = require('./modules/ask'),
-    askUser = require('./modules/askUser'),
-    _ = require("lodash"),
-    del = require('delete'),
-    symlinkOrCopySync = require('symlink-or-copy').sync,
-    package = require('./package.json'),
-    app = express();
+var express = require("express"),
+  fs = require("fs"),
+  walk = require("fs-walk"),
+  compression = require("compression"),
+  https = require("https"),
+  //expressHTTP2Workaround = require('express-http2-workaround'),
+  log4js = require("log4js"),
+  cors = require("./modules/cors"),
+  cache = require("./modules/cache"),
+  imageProxy = require("./endpoints/imageProxy"),
+  rescan = require("./endpoints/rescan"),
+  progress = require("./endpoints/progress"),
+  login = require("./endpoints/login"),
+  listen = require("./endpoints/listen"),
+  config = require("./config.json"),
+  ask = require("./modules/ask"),
+  askUser = require("./modules/askUser"),
+  _ = require("lodash"),
+  del = require("delete"),
+  symlinkOrCopySync = require("symlink-or-copy").sync,
+  package = require("./package.json"),
+  app = express();
 
 // set-up main logger
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file('logs/mp3stream.log'), 'mp3stream');
-var logger = log4js.getLogger('mp3stream');
-logger.setLevel('DEBUG');
+log4js.loadAppender("file");
+log4js.addAppender(log4js.appenders.file("logs/mp3stream.log"), "mp3stream");
+var logger = log4js.getLogger("mp3stream");
+logger.setLevel("DEBUG");
 
 // set-up express
-app.use(expressHTTP2Workaround({ express: express, http2: http2 }));
+// app.use(expressHTTP2Workaround({ express: express, http2: http2 }));
 app.use(compression());
 app.use(cors);
 app.use(cache);
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // check if config is set-up if not ask for input
 var addUserMode = process.argv[2] === "adduser";
 if (addUserMode) {
-    askUser();
-    return;
+  askUser();
+  return;
 }
 if (config.ask || !config.path) {
-    // ask user to set-up configuration
-    ask();
-    return;
+  // ask user to set-up configuration
+  ask();
+  return;
 } else {
-    // link website
-    if (config.useJSMusicDB) {
-        logger.info("Setting up website ...");
-        walk.files('node_modules/jsmusicdbnext-prebuilt/', (dir, file) => {
-            // link these files
-            del.sync('public/' + file);
-            symlinkOrCopySync('node_modules/jsmusicdbnext-prebuilt/' + file, 'public/' + file);
-            del.sync('public/global');
-            symlinkOrCopySync('node_modules/jsmusicdbnext-prebuilt/global', 'public/global');
-        });
-    }
-    // set-up endpoints
-    app.get('/data/image-proxy', imageProxy);
-    app.get('/listen', listen);
-    app.post('/login', login);
-    app.get('/rescan', rescan);
-    app.get('/progress', progress);
+  // link website
+  if (config.useJSMusicDB) {
+    logger.info("Setting up website ...");
+    walk.files("node_modules/jsmusicdbnext-prebuilt/", (dir, file) => {
+      // link these files
+      del.sync("public/" + file);
+      symlinkOrCopySync(
+        "node_modules/jsmusicdbnext-prebuilt/" + file,
+        "public/" + file
+      );
+      del.sync("public/global");
+      symlinkOrCopySync(
+        "node_modules/jsmusicdbnext-prebuilt/global",
+        "public/global"
+      );
+    });
+  }
+  // set-up endpoints
+  app.get("/data/image-proxy", imageProxy);
+  app.get("/listen", listen);
+  app.post("/login", login);
+  app.get("/rescan", rescan);
+  app.get("/progress", progress);
 }
 
 // redirect everything to index if not in predefined list
-app.get(/^(?!\/rescan|\/listen|\/progress|\/data\/.*|\/dist.*|\/global.*|\/dist-systemjs.*|\/sw.js|\/manifest.json|\/js\/.*|\/app.*|\/css\/.*|\/fonts\/.*|\/fonts\/glyphs\/.*).*$/, function(req, res) {
-    res.sendFile('public/index.html', { root: __dirname });
-});
+app.get(
+  /^(?!\/rescan|\/listen|\/progress|\/data\/.*|\/dist.*|\/global.*|\/dist-systemjs.*|\/sw.js|\/manifest.json|\/js\/.*|\/app.*|\/css\/.*|\/fonts\/.*|\/fonts\/glyphs\/.*).*$/,
+  function(req, res) {
+    res.sendFile("public/index.html", { root: __dirname });
+  }
+);
 
 // start-up express
 if (config.useSSL) {
-    var privateKey = fs.readFileSync(config.sslKey, 'utf8');
-    var certificate = fs.readFileSync(config.sslCert, 'utf8');
-    var credentials = {
-        key: privateKey,
-        cert: certificate
-    };
-    var httpsServer = http2.createServer(credentials, app);
-    httpsServer.listen(config.port);
-    logger.info(`node mp3stream ${package.version} is set-up and running in http/2 mode`);
+  var privateKey = fs.readFileSync(config.sslKey, "utf8");
+  var certificate = fs.readFileSync(config.sslCert, "utf8");
+  var credentials = {
+    key: privateKey,
+    cert: certificate
+  };
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(config.port);
+  logger.info(
+    `node mp3stream ${package.version} is set-up and running in https mode`
+  );
 } else {
-    app.listen(config.port);
-    logger.info(`node mp3stream ${package.version} is set-up and running in http mode`);
+  app.listen(config.port);
+  logger.info(
+    `node mp3stream ${package.version} is set-up and running in http mode`
+  );
 }
--- endpoints/imageProxy.js
+++ endpoints/imageProxy.js
@@ -1,63 +1,71 @@
-var request = require('request'),
-    log4js = require('log4js'),
-    fs = require('fs'),
-    url = require('url');
+var request = require("request"),
+  log4js = require("log4js"),
+  fs = require("fs"),
+  url = require("url");
 
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file('logs/mp3stream.log'), 'image-proxy');
-var logger = log4js.getLogger('image-proxy');
-logger.setLevel('INFO');
+log4js.loadAppender("file");
+log4js.addAppender(log4js.appenders.file("logs/mp3stream.log"), "image-proxy");
+var logger = log4js.getLogger("image-proxy");
+logger.setLevel("INFO");
 
-var serveFile = function (path, res) {
-    fs.readFile(path, function (err, buffer) {
-        res.writeHead(200, {
-            'Content-Length': Buffer.byteLength(buffer),
-            'Content-Type': 'image/png'
-        });
-        res.write(buffer, function () {
-            res.end();
-        })
+var serveFile = function(path, res) {
+  fs.readFile(path, function(err, buffer) {
+    res.writeHead(200, {
+      "Content-Length": Buffer.byteLength(buffer),
+      "Content-Type": "image/png"
     });
+    res.write(buffer, function() {
+      res.end();
+    });
+  });
 };
 
-var cacheFile = function (queryData, res, cacheName, cb) {
-    var cacheData = '';
-    if (queryData.url) {
-        request({
-            url: queryData.url
-        }).on('error', function (e) {
-            res.end(e);
-        }).on('end', function (e) {
-            if (cacheData) {
-                logger.info('write cache as ' + cacheName);
-                fs.writeFile('public/data/cache/' + cacheName, cacheData, 'binary', function (err, data) {
-                    if (err) {
-                        logger.error(err);
-                    } else {
-                        cb();
-                    }
-                });
+var cacheFile = function(queryData, res, cacheName, cb) {
+  var cacheData = "";
+  if (queryData.url) {
+    request({
+      url: queryData.url
+    })
+      .on("error", function(e) {
+        res.end(e);
+      })
+      .on("end", function(e) {
+        if (cacheData) {
+          logger.info("write cache as " + cacheName);
+          fs.writeFile(
+            "public/data/cache/" + cacheName,
+            cacheData,
+            "binary",
+            function(err, data) {
+              if (err) {
+                logger.error(err);
+              } else {
+                cb();
+              }
             }
-        }).on('response', function (r) {
-            r.setEncoding('binary');
-            r.on('data', function (chunk) {
-                cacheData += chunk;
-            });
+          );
+        }
+      })
+      .on("response", function(r) {
+        r.setEncoding("binary");
+        r.on("data", function(chunk) {
+          cacheData += chunk;
         });
-    }
+      });
+  }
 };
 
-module.exports = function (req, res) {
-    var queryData = url.parse(req.url, true).query;
-    var cacheName = queryData.url;
-    cacheName = cacheName.substr(cacheName.indexOf("300x300") + 7);
-    if (fs.existsSync('public/data/cache/' + cacheName)) {
-        // serve this file
-        serveFile('public/data/cache/' + cacheName, res);
-    } else {
-        // get file from internet, store it and then serve it.
-        cacheFile(queryData, res, cacheName, function () {
-            serveFile('public/data/cache/' + cacheName, res);
-        });
-    }
+module.exports = function(req, res) {
+  var queryData = url.parse(req.url, true).query;
+  var cacheName = queryData.url;
+  cacheName = cacheName.substr(cacheName.indexOf("300x300") + 7);
+  if (fs.existsSync("public/data/cache/" + cacheName)) {
+    // serve this file
+    serveFile("public/data/cache/" + cacheName, res);
+  } else {
+    // get file from internet, store it and then serve it.
+    cacheFile(queryData, res, cacheName, function() {
+      serveFile("public/data/cache/" + cacheName, res);
+    });
+  }
 };
