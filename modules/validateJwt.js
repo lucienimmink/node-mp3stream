322973eaa50f28bfa8eddd8da22b44c5dec75d5f
Lucien Immink <lucien@arielext.org>
2021-08-02 15:31:07 +0200

    encrypt passwords in payload

--- modules/validateJwt.js
+++ modules/validateJwt.js
@@ -1,7 +1,37 @@
+const fs = require("fs");
+const { Crypto } = require("@peculiar/webcrypto");
 var jwt = require("jwt-simple"),
   db = require("./db");
 
+
 var knownJWTTokens = {};
+
+const crypto = new Crypto();
+
+const decryptPassword = async (encrypted) => {
+  const payloadBuffer = Buffer.from(encrypted, "base64");
+  const privateKeyJSON = JSON.parse(
+    fs.readFileSync("./.private-key.json", "utf8")
+  );
+  const privateKey = await crypto.subtle.importKey(
+    "jwk",
+    privateKeyJSON,
+    {
+      name: "RSA-OAEP",
+      hash: "SHA-256"
+    },
+    false,
+    ["decrypt"]
+  );
+  const data = await crypto.subtle.decrypt(
+    {
+      name: "RSA-OAEP"
+    },
+    privateKey,
+    payloadBuffer
+  );
+  return new TextDecoder().decode(data);
+};
 
 var parseToken = function(token) {
   if (!token) {
@@ -14,13 +44,18 @@ var parseToken = function(token) {
   return decoded;
 };
 
-module.exports = function(jwt, cb) {
+module.exports = async function(jwt, cb) {
   if (!knownJWTTokens[jwt]) {
     var decoded = parseToken(jwt);
     if (!decoded) {
       cb(false);
     }
-    db(decoded.name, decoded.password, cb, jwt, knownJWTTokens);
+    // for now we keep support for unencrypted passwords as well; for now :)
+    let password = decoded.password;
+    if (decoded.encryptedPassword) {
+      password = await decryptPassword(decoded.encryptedPassword);
+    }
+    db(decoded.name, password, cb, jwt, knownJWTTokens);
   } else {
     cb(true);
   }
