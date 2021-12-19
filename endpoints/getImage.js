688d3c228e470426a09acf89f86e248517741352
Lucien Immink <lucien@arielext.org>
2021-12-19 22:00:35 +0100

    update API; return the URL to the image

--- endpoints/getImage.js
+++ endpoints/getImage.js
@@ -3,19 +3,6 @@ const logger = require("../modules/logger")("getImage");
 const fs = require("fs");
 const url = require("url");
 
-const serveFile = (path, res) => {
-  logger.info(`Serving cached image for ${path}`);
-  fs.readFile(path, (err, buffer) => {
-    res.writeHead(200, {
-      "Content-Length": Buffer.byteLength(buffer),
-      "Content-Type": "image/png",
-    });
-    res.write(buffer, () => {
-      res.end();
-    });
-  });
-};
-
 const cacheFile = (queryData, res, cacheName, cb) => {
   logger.info(`Creating cached image for ${cacheName}`);
   let cacheData = "";
@@ -52,16 +39,25 @@ const cacheFile = (queryData, res, cacheName, cb) => {
   }
 };
 
+const returnPath = (path, req, res) => {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(JSON.stringify({
+    url: `//${req.headers.host}/data/cache/${path}`
+  }));
+}
+
 module.exports = (req, res) => {
   const queryData = url.parse(req.url, true).query;
   const mbid = queryData.mbid;
   if (mbid) {
-    if (fs.existsSync(`public/data/cache/${cacheName}`)) {
-      serveFile(`public/data/cache/${mbid}`, res);
-      return;
+    const files = fs.readdirSync(`public/data/cache`);
+    const matchedFiles = files.filter(file => file.includes(mbid));
+    if (matchedFiles.length > 0) {
+      returnPath(matchedFiles[0], req, res);
+    } else {
+      res.writeHead(404, "Not found");
+      res.end();
     }
-    res.writeHead(404, "Not found");
-    res.end();
     return;
   }
   let cacheName = queryData.url;
@@ -73,10 +69,10 @@ module.exports = (req, res) => {
   }
   cacheName = cacheName.replace(/\//g,`_`);
   if (fs.existsSync(`public/data/cache/${cacheName}`)) {
-    serveFile(`public/data/cache/${cacheName}`, res);
+    returnPath(cacheName, req, res);
   } else {
     cacheFile(queryData, res, cacheName, () => {
-      serveFile(`public/data/cache/${cacheName}`, res);
+      returnPath(cacheName, req, res);
     });
   }
 };
