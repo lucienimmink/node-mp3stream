822f850cc83620511f0f582f5696916de288e26e
Lucien Immink <lucien@arielext.org>
2023-02-28 15:30:56 +0100

    Drop request, compress RSS feed response

--- endpoints/getImage.js
+++ endpoints/getImage.js
@@ -1,63 +1,31 @@
-const request = require("request");
 const logger = require("../modules/logger")("getImage");
 const fs = require("fs");
 const url = require("url");
 
-const cacheFile = (queryData, res, cacheName, cb) => {
-  logger.info(`Creating cached image for ${cacheName}`);
-  let cacheData = "";
-  if (queryData.url) {
-    request({
-      url: queryData.url,
-    })
-      .on("error", (e) => {
-          logger.error(`Error creating cached image for ${cacheName}`, e);
-        res.end(e);
-      })
-      .on("end", (e) => {
-        if (cacheData) {
-          fs.writeFile(
-            `public/data/cache/${cacheName}`,
-            cacheData,
-            "binary",
-            (err, data) => {
-              if (err) {
-                logger.error(`Error creating cached image for ${cacheName}`, err);
-              } else {
-                cb();
-              }
-            }
-          );
-        }
-      })
-      .on("response", (r) => {
-        r.setEncoding("binary");
-        r.on("data", (chunk) => {
-          cacheData += chunk;
-        });
-      });
-  }
-};
-
 const returnPath = (path, req, res) => {
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.end(JSON.stringify({
-    url: `//${req.headers.host}/data/cache/${path}`
-  }));
-}
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      url: `//${req.headers.host}/data/cache/${path}`,
+    })
+  );
+};
 
 module.exports = (req, res) => {
   const queryData = url.parse(req.url, true).query;
   const mbid = queryData.mbid;
   if (mbid) {
     const files = fs.readdirSync(`public/data/cache`);
-    const matchedFiles = files.filter(file => file.includes(mbid));
+    const matchedFiles = files.filter((file) => file.includes(mbid));
     if (matchedFiles.length > 0) {
       returnPath(matchedFiles[0], req, res);
     } else {
       // don't cache 404 results please
       res.removeHeader("Cache-Control");
-      res.setHeader("Expires", new Date(new Date().getTime() - 1).toUTCString());
+      res.setHeader(
+        "Expires",
+        new Date(new Date().getTime() - 1).toUTCString()
+      );
       res.writeHead(404, "Not found");
       res.end();
     }
@@ -65,17 +33,23 @@ module.exports = (req, res) => {
   }
   let cacheName = queryData.url;
   if (cacheName.includes("https://")) {
-    cacheName = cacheName.substring(8)
+    cacheName = cacheName.substring(8);
   }
   if (cacheName.includes("http://")) {
-    cacheName = cacheName.substring(7)
+    cacheName = cacheName.substring(7);
   }
-  cacheName = cacheName.replace(/\//g,`_`);
+  cacheName = cacheName.replace(/\//g, `_`);
   if (fs.existsSync(`public/data/cache/${cacheName}`)) {
+    logger.debug('returning cached image')
     returnPath(cacheName, req, res);
   } else {
-    cacheFile(queryData, res, cacheName, () => {
-      returnPath(cacheName, req, res);
-    });
+    logger.debug('no cached image found')
+    res.removeHeader("Cache-Control");
+    res.setHeader(
+      "Expires",
+      new Date(new Date().getTime() - 1).toUTCString()
+    );
+    res.writeHead(404, "Not found");
+    res.end();
   }
 };
--- endpoints/proxy.js
+++ endpoints/proxy.js
@@ -1,30 +1,42 @@
-const request = require("request");
+const https = require("https");
+const http = require("https");
 const logger = require("../modules/logger")("proxy");
 const validateJwt = require("./../modules/validateJwt");
 const url = require("url");
 
 module.exports = (req, res) => {
-    const queryData = url.parse(req.url, true).query;
-    const { remote, jwt } = queryData;
-    validateJwt(jwt, function (val) {
-        if (val) {
-            if (remote) {
-                logger.debug(`fetching ${remote}`);
-                request({
-                    url: remote,
-                    method: 'GET'
-                }, function (error, response, body) {
-                    res.write(body);
-                    res.end();
-                })
-            } else {
-                logger.warn('No remote given');
-                res.end();
-            }
-        } else {
-            res.statusCode = 401;
+  const queryData = url.parse(req.url, true).query;
+  const { remote, jwt } = queryData;
+  validateJwt(jwt, function (val) {
+    if (val) {
+      if (remote) {
+        const type = remote.startsWith("https") ? https : http;
+        logger.debug(`fetching ${remote}`);
+        const re = type.get(remote, (resp) => {
+          let data = "";
+          console.log(resp.headers);
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+          resp.on("end", () => {
+            res.writeHead(200, { 'content-type': resp.headers["content-type"] })
+            res.write(data);
             res.end();
-        }
-    });
-    
+          });
+        });
+        re.on("error", (err) => {
+          logger.error(`Remote (${remote}) returned an error: ${err}`);
+          res.statusCode = 400;
+          res.end();
+        });
+      } else {
+        logger.warn("No remote given");
+        res.statusCode = 400;
+        res.end();
+      }
+    } else {
+      res.statusCode = 401;
+      res.end();
+    }
+  });
 };
